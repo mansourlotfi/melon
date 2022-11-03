02c769dea0c4b33fc12a7d9fd2f67b20945acf52
mansourlotfi <mansourlotfi@gmail.com>
2022-11-03 17:42:40 +0330

    Add Rtl layout to mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,24 @@ import createEmotionCache from "../utility/createEmotionCache";
 import { StoreProvider } from "../utility/Store";
 import lightTheme from "../styles/theme/lightTheme";
 import "../styles/globals.css";
-
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+// import { prefixer } from 'stylis';
 const clientSideEmotionCache = createEmotionCache();
+
+// const cacheRtl = createCache({
+//   key: 'muirtl',
+//   stylisPlugins: [prefixer, rtlPlugin],
+// });
+
+const cacheRtl = createCache({ key: "muirtl", stylisPlugins: [rtlPlugin] });
 
 const MyApp = (props: any) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
     <StoreProvider>
-      <CacheProvider value={emotionCache}>
+      <CacheProvider value={cacheRtl}>
         <ThemeProvider theme={lightTheme}>
           <CssBaseline />
           <Component {...pageProps} />
