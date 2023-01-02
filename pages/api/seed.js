90baf60937193aed09ebcc18e04a16591987bb49
mansourlotfi <mansourlotfi@gmail.com>
2023-01-02 13:16:02 +0330

    minor fix on seed data

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,5 +1,5 @@
 import nc from "next-connect";
-import Product from "../../models/Product";
+// import Product from "../../models/Product";
 import User from "../../models/User";
 import db from "../../utility/db";
 import data from "../../utility/data";
@@ -9,8 +9,8 @@ handler.get(async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
-  await Product.deleteMany();
-  await Product.insertMany(data.products);
+  // await Product.deleteMany();
+  // await Product.insertMany(data.products);
 
   await db.disconnect();
   res.send({ message: "Seeded successfully" });
